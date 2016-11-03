091fcca0b9b4ee880adb0cb7aa78db410839681e
sauyee <sauyee333@gmail.com>
2016-11-04 07:55:40 +0800

    disable http log

--- app/src/main/java/com/sauyee333/herospin/utils/Constants.java
+++ app/src/main/java/com/sauyee333/herospin/utils/Constants.java
@@ -26,6 +26,8 @@ public class Constants {
     public static final String MARVEL_IMAGE_LANDSCAPE_AMAZING = "landscape_amazing"; //250x156
     public static final String MARVEL_IMAGE_LANDSCAPE_INCREDIBLE = "standard_incredible"; //464x261
 
+    public static final boolean HTTP_LOG_ENABLED = false;
+
     public static final String BUNDLE_STRING_MOVIE_INFO = "movieInfo";
     public static final String BUNDLE_STRING_HERO = "hero";
     public static final String BUNDLE_STRING_TYPE = "type";
